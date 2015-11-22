ef73e0a18fe8ae4bb44f5257818546c7c577cc36
Timothee 'TTimo' Besset <ttimo@ttimo.net>
2015-11-22 08:48:46 -0600

    adapting for server use

--- cloud-init/refresh-user-data-payload.sh
+++ cloud-init/refresh-user-data-payload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 # NOTE: this generates a new file every time, even if content hasn't changed
 # (mime multipart uses some random uuid between the parts)
+# If you need run-parts, it's in debianutils package (even available on macports)
 
 echo "Updating user-data.payload.gz"
 
--- salt/top.sls
+++ salt/top.sls
@@ -2,7 +2,6 @@ base:
   '*':
     - general
     - emacs
-    # package has a dependency problem on ubuntu 15 apparently
     - mosh
     - fish
     # git clone https://github.com/ahdinosaur-os/google-chrome-formula to /srv/formula

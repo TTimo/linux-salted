336b34517242e4f67e66bdec4093d1b1d6b26af3
TTimo <ttimo@ttimo.net>
2015-12-13 21:44:17 -0600

    xmonad configuration tweaks .. more questions than solutions

--- salt/xmonad/xmonad.hs
+++ salt/xmonad/xmonad.hs
@@ -3,11 +3,14 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.GroupNavigation
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Gaps
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+
+-- TODO: how to show xmobar on both displays?
 
 main = do
   xmproc <- spawnPipe "xmobar /home/timo/.xmonad/xmobarrc"
@@ -16,8 +19,12 @@ main = do
     terminal = "urxvt",
     manageHook = manageDocks <+> manageHook defaultConfig,
     layoutHook = avoidStruts $ layoutHook defaultConfig,
-    logHook = dynamicLogWithPP $ xmobarPP
+		-- TODO: can this syntax be cleaned up
+    logHook = ( dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc,
                           ppTitle = xmobarColor "green" "" . shorten 50
-                        }
+                        } ) >> historyHook
   }
+
+-- TODO: add this key .. how
+myKeys = [ ("M-x", nextMatch History (return True)) ]

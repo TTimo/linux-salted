14f409372079dc576e006ba97a8ab5e659a6d579
TTimo <ttimo@ttimo.net>
2018-02-09 09:32:43 -0600

    revisit the cloud-init on EC2 business

--- cloud-init/refresh-user-data-payload.sh
+++ cloud-init/refresh-user-data-payload.sh
@@ -3,8 +3,10 @@
 # (mime multipart uses some random uuid between the parts)
 # If you need run-parts, it's in debianutils package (even available on macports)
 
-echo "Updating user-data.payload.gz"
+# How to use this in EC2:
+# Under 'Instance Details', 'Advanced Details' supply the user data as file, and mark it 'already base64 encoded'
 
-cd scripts
-run-parts --list . | xargs ../write-mime-multipart -z > ../user-data.payload.gz
+# cloud-init fails if you don't base64 encode, possibly because EC2 prepends it's own user data?
+# see https://github.com/terraform-providers/terraform-provider-aws/issues/183
 
+run-parts --list scripts | xargs ./write-mime-multipart -z | base64 > user-data.payload.gz.base64
